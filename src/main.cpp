c0481a307f353b53bdbc943e630596404e7ac521
umarnurmatov <umarkhonnur12@gmail.com>
2023-08-30 21:11:30 +0300

    gps added

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,166 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
-#include <RTClib.h>
-#include "GyverOLED.h"
+
 #include <map>
 #include <vector>
+
+#include "GyverOLED.h"
 #include "Button.h"
+#include "TinyGPSPlus.h"
 
-#define WRITE_PERIOD 3000
-#define RTC_SETUP_WAIT_TIME 10000
+///////////////////////////////////
+#define SERIAL_BAUD         115200
 
-RTC_DS3231 rtc;
-Button btn1 {15, false};
+#define FILE_WRITE_PERIOD   3000
+
+#define GPS_RX_PIN          2
+#define GPS_TX_PIN          4
+#define GPS_BAUD            9600
+#define GPS_MAX_WAIT_TIME   5000
+#define GPS_FEED_TIME       1000 
+
+#define PM_SENSOR_BAUD      9600
+
+#define SHUTDOWN_BUTTON_PIN 15 
+///////////////////////////////////
+
+
+/////////////////////////////////////////////////
+Button shutdownBtn {15, false};
 GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
+TinyGPSPlus gps;
 String filename;
+/////////////////////////////////////////////////
 
-bool initSD();
-bool initRTC();
-void initFile();
-void shutDown();
+
+////////////////////////
+static bool initSD();
+static bool initGPS();
+static bool initFile();
+static void shutDown();
 
 bool get_pm(float& pm_10, float& pm_25);
 
-/// @brief DD-MM-YYYY,hh:mm:ss"
-/// @return current datetime of format ^
-String get_datetime();
+static void feedGpsSomeData(unsigned long ms);
+static bool isGPSDataValid();
 
-void print_oled(const char* msg, int y = 1, int x = 1, bool clear = true, bool autoprint = true);
-void writeFile(const char* path, String& message);
+static void print_oled(const char* msg, int y = 1, int x = 1, bool clear = true, bool autoprint = true);
+static void writeFile(const char* path, String& message);
 
 void IRAM_ATTR isr_button();
+///////////////////////
+
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    Serial2.begin(9600, SERIAL_8N1, -1, -1, false);
+    Serial.begin(SERIAL_BAUD);
+    Serial1.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+    Serial2.begin(PM_SENSOR_BAUD, SERIAL_8N1, -1, -1, false);
+    
+    pinMode(shutdownBtn.PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(shutdownBtn.PIN), isr_button, RISING);
 
+    /////////////////
     oled.init();
-    if (!initSD() || !initRTC())
-        while (true);
 
-    pinMode(btn1.PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(btn1.PIN), isr_button, RISING);
+    if (!initSD())
+        while(true);
 
-    initFile();
+    if(!initGPS())
+        while(true);
 
+    if(!initFile())
+        while(true);
+    ////////////////
+
+    Serial.println("Init success!");
     print_oled("INIT SUCCESS!");
 }
+
 
 void loop() {
     float pm_10 = 0.f, pm_25 = 0.f;
     static unsigned long timet = 0;
-    if(btn1.pressed)
+    if(shutdownBtn.pressed)
     {
         shutDown();
     }
-    if (get_pm(pm_10, pm_25)) {
-        Serial.printf("PM2.5 = %.4f; PM10 = %.4f\n", pm_25, pm_10);
-        //dd-mm-yyyy,hh:mm:ss,PM2.5,PM10
-        if (millis() - timet >= WRITE_PERIOD) {
-            String strdata = get_datetime();
+
+    feedGpsSomeData(GPS_FEED_TIME);
+
+    if (get_pm(pm_10, pm_25) && isGPSDataValid()) {
+        //"dd-mm-yyyy,hh:mm:ss,PM2.5,PM10,lat,lng,alt"
+        //the "\n" is added in writeFile()
+        if (millis() - timet >= FILE_WRITE_PERIOD) {
+            String strdata;
+            strdata += String(gps.date.day());
+            strdata += "-";
+            strdata += String(gps.date.month());
+            strdata += "-";
+            strdata += String(gps.date.year());
+            strdata += ",";
+            strdata += String(gps.time.hour());
+            strdata += ":";
+            strdata += String(gps.time.minute());
+            strdata += ":";
+            strdata += String(gps.time.second());
             strdata += ",";
             strdata += String(pm_25);
             strdata += ",";
             strdata += String(pm_10);
+            strdata += ",";
+            strdata += String(gps.location.lat(), 6U);
+            strdata += ",";
+            strdata += String(gps.location.lng(), 6U);
+            strdata += ",";
+            strdata += String(gps.altitude.meters(), 2U);
+
+            String dispdata;
+            dispdata += "DATE ";
+            dispdata += String(gps.date.day());
+            dispdata += "-";
+            dispdata += String(gps.date.month());
+            dispdata += "-";
+            dispdata += String(gps.date.year());
+            print_oled(dispdata.c_str(), 1, 1, true, false);
+            dispdata.clear();
+
+            dispdata += "TIME ";
+            dispdata += String(gps.time.hour());
+            dispdata += ":";
+            dispdata += String(gps.time.minute());
+            dispdata += ":";
+            dispdata += String(gps.time.second());
+            print_oled(dispdata.c_str(), 2, 1, false, false);
+            dispdata.clear();
+
+            dispdata += "PM ";
+            dispdata += String(pm_25, 1U);
+            dispdata += ",";
+            dispdata += String(pm_10, 1U);
+            print_oled(dispdata.c_str(), 3, 1, false, false);
+            dispdata.clear();
+
+            dispdata += String(gps.location.lat(), 4U) + "N";
+            dispdata += " ";
+            dispdata += String(gps.location.lng(), 4U) + "E";
+            print_oled(dispdata.c_str(), 4, 1, false, false);
+            dispdata.clear();
+
+
+            dispdata += "ALT " + String(gps.altitude.meters(), 2U) + "m";
+            print_oled(dispdata.c_str(), 5, 1, false, false);
+            dispdata.clear();
+
             Serial.println(strdata);
-            print_oled(strdata.c_str());
             writeFile(filename.c_str(), strdata);
             timet = millis();
         }
     }
+
 }
 
-bool initSD() 
+static bool initSD() 
 {
     if (!SD.begin()) 
     {
@@ -88,96 +176,66 @@ bool initSD()
         return false;
     }
 
-    Serial.print("SD Card Type: ");
-    switch(cardType)
-    {
-        case CARD_MMC:
-        Serial.println("MMC");
-        break;
-        case CARD_SD:
-        Serial.println("SD");
-        break;
-        case CARD_SDHC:
-        Serial.println("SDHC");
-        break;
-        default:
-        Serial.println("UNKNOWN");
-        break;
-    }
-
-
     uint64_t cardSize = SD.cardSize() / (1024 * 1024);
     Serial.printf("SD Card Size: %lluMB\n", cardSize);
     print_oled("SD INIT SUCCESS");
     return true;
 }
 
-bool initRTC() 
+static bool initGPS()
 {
-    if (!rtc.begin()) {
-        Serial.println("RTC start fail");
-        print_oled("RTC INIT FAIL");
-        return false;
-    }
-
-    Serial.println("RTC started");
-    Serial.printf("RTC lost power: %s\n", rtc.lostPower() ? "yes" : "no");
-
-    String choice = "N";
-
     unsigned long timer = millis();
-    Serial.print("Wanna setup date&time? Y/N: ");
-    while(!Serial.available() && (millis() - timer < RTC_SETUP_WAIT_TIME)) {}
-    if(Serial.available()) choice = Serial.readStringUntil('\n');
-    Serial.println(choice);
-
-    if (choice == "Y") {
-        Serial.println("rtc lost power, please setup date&time");
-        std::map<String, uint16_t> m;
-        static String s[] = {
-            "day",
-            "month",
-            "year",
-            "hour",
-            "minute",
-            "second",
-        };
-        for (auto& i : s) {
-            Serial.printf("Enter %s:", i.c_str());
-            while (!Serial.available());
-            String inp = Serial.readStringUntil('\n');
-            m[i] = inp.toInt();
-            Serial.println(inp);
+    print_oled("WAITING GPS FIX...");
+    Serial.println("Waiting GPS data");
+    while(true)
+    {
+        feedGpsSomeData(GPS_FEED_TIME);
+        if(isGPSDataValid()) break;
+        if ((timer - millis() > GPS_MAX_WAIT_TIME) && gps.charsProcessed() < 10)
+        {
+            print_oled("NO GPS DATA RECEIVEDCHECK WIRING");
+            Serial.println(F("No GPS data received: check wiring"));
+            return false;
         }
-        DateTime dt(m["year"], m["month"], m["day"], m["hour"], m["minute"], m["second"]);
-        rtc.adjust(dt);
-        Serial.printf("Adjusted time is: %s\n", get_datetime().c_str());
     }
-    else
-        Serial.println("skipped");
-
-    print_oled("RTC INIT SUCCESS");
+    print_oled("GPS GOT 3D FIX");
+    Serial.println("GPS got 3D fix");
     return true;
 }
 
-void initFile()
+// <MM-DD-YYYY$hh-mm-ss.csv>
+static bool initFile()
 {
-    DateTime now = rtc.now();
-    static char buf[] = "MM-DD-YYYY$hh-mm-ss";
-    filename = String("/") + String(now.toString(buf)) + String(".txt");
+    feedGpsSomeData(GPS_FEED_TIME); 
+    if(!isGPSDataValid())
+    {
+        print_oled("FAIL INIT FILE: GPS INVALID");
+        Serial.println("Fail init file: GPS data not valid");
+        return false;
+    } 
+    TinyGPSDate date = gps.date;
+    TinyGPSTime time = gps.time;
+    
+    filename = String("/") + String(date.month()) + String("-") + String(date.day()) + String("-") 
+                + String(date.year()) + String("$") + String(time.hour())+ String("-") + String(time.minute())
+                + String("-") + String(time.second()) + String(".csv");
+
     if(!SD.exists(filename))
     {
         File file = SD.open(filename, FILE_WRITE, true);
-        file.println("date,time,PM2.5,PM10");
+        file.println("date,time,PM2.5,PM10,lat,lng,alt");
         file.close();
     }
+    return true;
 }
 
-void shutDown()
+static void shutDown()
 {
     Serial.println("Shut down");
     print_oled("SHUT DOWN");
     SD.end();
+    Serial.end();
+    Serial1.end();
     Serial2.end();
     while(true);
 }
@@ -203,10 +261,20 @@ bool get_pm(float& pm_10, float& pm_25) {
         return false;
 }
 
-String get_datetime() {
-    DateTime now = rtc.now();
-    char buf[] = "DD-MM-YYYY,hh:mm:ss";
-    return String(now.toString(buf));
+static void feedGpsSomeData(unsigned long ms)
+{
+    unsigned long start = millis();
+    do 
+    {
+        while (Serial1.available()) 
+            gps.encode(Serial1.read());
+    } while (millis() - start < ms);
+}
+
+bool isGPSDataValid()
+{
+    if(gps.location.isValid() && gps.time.isValid() && gps.date.isValid() && gps.altitude.isValid()) return true;
+    return false;
 }
 
 void print_oled(const char* msg, int y, int x, bool clear, bool autoprint)
@@ -218,25 +286,25 @@ void print_oled(const char* msg, int y, int x, bool clear, bool autoprint)
     oled.update();
 }
 
-void writeFile(const char* path, String& message) {
+static void writeFile(const char* path, String& message) {
     Serial.printf("Writing file: %s\n", path);
 
     File file = SD.open(path, FILE_APPEND);
     if (!file) 
     {
         Serial.println("Failed to open file for writing");
-        print_oled("FAILED OPEN FILE FOR WRITE", 4, 1, false);
+        print_oled("FAILED OPEN FILE FOR WRITE", 1, 1, true);
         while(true);
     }
     if (file.println(message)) 
     {
         Serial.println("File written");
-        print_oled("FILE WRITTEN", 4, 1, false);
+        print_oled("FILE WRITTEN", 6, 1, false);
     } 
     else 
     {
         Serial.println("Write failed");
-        print_oled("WRITE FAIL", 4, 1, false);
+        print_oled("WRITE FAIL", 1, 1, true);
         file.close();
         while(true);
     }
@@ -246,9 +314,9 @@ void writeFile(const char* path, String& message) {
 void IRAM_ATTR isr_button()
 {
     unsigned long time = millis();
-    if (time - btn1.last_debounce_time > 250)
+    if (time - shutdownBtn.last_debounce_time > 250)
     {
-        btn1.pressed = true;
-        btn1.last_debounce_time = time;
+        shutdownBtn.pressed = true;
+        shutdownBtn.last_debounce_time = time;
     }
 }
