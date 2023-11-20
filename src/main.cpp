70ad797dfc54000c777a5a73ff886398ae563333
umarnurmatov <umarkhonnur12@gmail.com>
2023-11-20 23:33:15 +0300

    added gps variety

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
-#include "Wire.h"
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <WiFi.h>
 
 #include "GyverOLED.h"
 #include "Button.h"
 #include "TinyGPSPlus.h"
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BME280.h"
+#include "ESP-FTP-Server-Lib.h"
+#include "FTPFilesystem.h"
 
 ///////////////////////////////////
 #define SERIAL_BAUD         115200
@@ -22,20 +25,38 @@
 
 #define PM_SENSOR_BAUD      9600
 
-#define SHUTDOWN_BUTTON_PIN 15
+#define BUTTON_1_PIN 15
+#define BUTTON_2_PIN 34
 
 #define SEALEVELPRESSURE_HPA 1013.25f
 
 #define IS_BME_PRESENT false
+#define IS_GPS_PRESENT false
 ///////////////////////////////////
+
+// WIFI SETTINGS //////////////////////
+const char* ssid     = "ESP32_AEROSOL";
+const char* password = "xtcf7080";
+
+IPAddress local_ip(192,168,0,1);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+///////////////////////////////////////
+
+// FTP SETTINGS //////////////// 
+#define FTP_USER     "ESP32_FTP"
+#define FTP_PASSWORD "xtcf7080"
+////////////////////////////////
 
 
 /////////////////////////////////////////////////
-Button shutdownBtn {SHUTDOWN_BUTTON_PIN, false};
+Button btn1 {BUTTON_1_PIN, false};
 GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
 TinyGPSPlus gps;
 Adafruit_BME280 bme;
 String filename;
+FTPServer ftpServer;
+int a = 0;
 /////////////////////////////////////////////////
 
 
@@ -57,17 +78,25 @@ static void print_oled(const char* msg, int y = 1, int x = 1, bool clear = true,
 static void writeFile(const char* path, String& message);
 
 void IRAM_ATTR isr_button();
+
+void setupWiFi();
+bool initFTP();
 ///////////////////////
 
 
 void setup() 
 {
     Serial.begin(SERIAL_BAUD);
-    Serial1.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+    if(IS_GPS_PRESENT) Serial1.begin(GPS_BAUD, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
     Serial2.begin(PM_SENSOR_BAUD, SERIAL_8N1, -1, -1, false);
     
-    pinMode(shutdownBtn.PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(shutdownBtn.PIN), isr_button, RISING);
+    pinMode(btn1.PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(btn1.PIN), isr_button, FALLING);
+
+
+    // setupWiFi();
+    // if(!initFTP())
+        // while(true);
 
     /////////////////
     oled.init();
@@ -79,12 +108,14 @@ void setup()
     if (!initSD())
         while(true);
 
-    if(!initGPS())
-        while(true);
+    if(IS_GPS_PRESENT)
+        if(!initGPS())
+            while(true);
 
     if(!initFile())
         while(true);
     ////////////////
+
 
     Serial.println("Init success!");
     print_oled("INIT SUCCESS!");
@@ -97,12 +128,13 @@ void loop()
     float bmeAlttitude = 0.f, humidity = 0.f, pressure = 0.f, temperature = 0.f;
     static unsigned long timet = 0;
 
-    if(shutdownBtn.pressed)
+    if(btn1.pressed)
         shutDown();
 
-    feedGpsSomeData(GPS_FEED_TIME);
+    if(IS_GPS_PRESENT)
+        feedGpsSomeData(GPS_FEED_TIME);
 
-    if (readPM(pm10, pm25) && isGPSDataValid()) 
+    if (readPM(pm10, pm25) && (!IS_GPS_PRESENT || isGPSDataValid())) 
     {
         if(IS_BME_PRESENT) readBME(bmeAlttitude, humidity, temperature, pressure);
 
@@ -110,75 +142,99 @@ void loop()
         //the "\n" is added in writeFile()
         if (millis() - timet >= FILE_WRITE_PERIOD) 
         {
+            Serial.println(a);
             String strdata;
-            strdata += String(gps.date.day());
-            strdata += "-";
-            strdata += String(gps.date.month());
-            strdata += "-";
-            strdata += String(gps.date.year());
-            strdata += ",";
-            strdata += String(gps.time.hour());
-            strdata += ":";
-            strdata += String(gps.time.minute());
-            strdata += ":";
-            strdata += String(gps.time.second());
-            strdata += ",";
+            if(IS_GPS_PRESENT)
+            {
+                strdata += String(gps.date.day());
+                strdata += "-";
+                strdata += String(gps.date.month());
+                strdata += "-";
+                strdata += String(gps.date.year());
+                strdata += ",";
+                strdata += String(gps.time.hour());
+                strdata += ":";
+                strdata += String(gps.time.minute());
+                strdata += ":";
+                strdata += String(gps.time.second());
+                strdata += ",";
+            }
+            else 
+                strdata += "NONE-NONE-NONE,NONE:NONE:NONE,"; 
             strdata += String(pm25);
             strdata += ",";
             strdata += String(pm10);
             strdata += ",";
-            strdata += String(gps.location.lat(), 6U);
-            strdata += ",";
-            strdata += String(gps.location.lng(), 6U);
-            strdata += ",";
-            strdata += String(gps.altitude.meters(), 2U);
-            strdata += ",";
-            if(IS_BME_PRESENT) strdata += String(bmeAlttitude) + "," + String(humidity) + "," + String(pressure) + "," + String(temperature);
-            else strdata += "NONE,NONE,NONE,NONE";
+            if(IS_GPS_PRESENT)
+            {
+                strdata += String(gps.location.lat(), 6U);
+                strdata += ",";
+                strdata += String(gps.location.lng(), 6U);
+                strdata += ",";
+                strdata += String(gps.altitude.meters(), 2U);
+                strdata += ",";
+            }
+            else 
+                strdata += "NONE,NONE,NONE,";
+            if(IS_BME_PRESENT) 
+                strdata += String(bmeAlttitude) + "," + String(humidity) + "," + String(pressure) + "," + String(temperature);
+            else 
+                strdata += "NONE,NONE,NONE,NONE";
 
             String dispdata;
-            dispdata += "DATE ";
-            dispdata += String(gps.date.day());
-            dispdata += "-";
-            dispdata += String(gps.date.month());
-            dispdata += "-";
-            dispdata += String(gps.date.year());
-            print_oled(dispdata.c_str(), 1, 1, true, false);
-            dispdata.clear();
+            if(IS_GPS_PRESENT)
+            {
+                dispdata += "DATE ";
+                dispdata += String(gps.date.day());
+                dispdata += "-";
+                dispdata += String(gps.date.month());
+                dispdata += "-";
+                dispdata += String(gps.date.year());
 
-            dispdata += "TIME ";
-            dispdata += String(gps.time.hour());
-            dispdata += ":";
-            dispdata += String(gps.time.minute());
-            dispdata += ":";
-            dispdata += String(gps.time.second());
-            print_oled(dispdata.c_str(), 2, 1, false, false);
-            dispdata.clear();
+                print_oled(dispdata.c_str(), 1, 1, true, false);
+                dispdata.clear();
 
-            dispdata += "PM ";
-            dispdata += String(pm25, 1U);
-            dispdata += ",";
-            dispdata += String(pm10, 1U);
-            print_oled(dispdata.c_str(), 3, 1, false, false);
-            dispdata.clear();
+                dispdata += "TIME ";
+                dispdata += String(gps.time.hour());
+                dispdata += ":";
+                dispdata += String(gps.time.minute());
+                dispdata += ":";
+                dispdata += String(gps.time.second());
+                print_oled(dispdata.c_str(), 2, 1, false, false);
+                dispdata.clear();
 
-            dispdata += String(gps.location.lat(), 4U) + "N";
-            dispdata += " ";
-            dispdata += String(gps.location.lng(), 4U) + "E";
-            print_oled(dispdata.c_str(), 4, 1, false, false);
-            dispdata.clear();
+                dispdata += "PM ";
+                dispdata += String(pm25, 1U);
+                dispdata += ",";
+                dispdata += String(pm10, 1U);
+                print_oled(dispdata.c_str(), 3, 1, false, false);
+                dispdata.clear();
 
+                dispdata += String(gps.location.lat(), 4U) + "N";
+                dispdata += " ";
+                dispdata += String(gps.location.lng(), 4U) + "E";
+                print_oled(dispdata.c_str(), 4, 1, false, false);
+                dispdata.clear();
 
-            dispdata += "ALT " + String(gps.altitude.meters(), 2U) + "m";
-            print_oled(dispdata.c_str(), 5, 1, false, false);
-            dispdata.clear();
+                dispdata += "ALT " + String(gps.altitude.meters(), 2U) + "m";
+                print_oled(dispdata.c_str(), 5, 1, false, false);
+                dispdata.clear();
+            }
+            else
+            {
+                dispdata += "PM ";
+                dispdata += String(pm25, 1U);
+                dispdata += ",";
+                dispdata += String(pm10, 1U);
+                print_oled(dispdata.c_str(), 1, 1, false, false);
+            }
 
             Serial.println(strdata);
             writeFile(filename.c_str(), strdata);
             timet = millis();
         }
     }
-
+    // ftpServer.handle();
 }
 
 static bool initSD() 
@@ -239,27 +295,35 @@ static bool initGPS()
 // <MM-DD-YYYY$hh-mm-ss.csv>
 static bool initFile()
 {
-    feedGpsSomeData(GPS_FEED_TIME); 
-    if(!isGPSDataValid())
+    if(IS_GPS_PRESENT)
     {
-        print_oled("FAIL INIT FILE: GPS INVALID");
-        Serial.println("Fail init file: GPS data not valid");
-        return false;
-    } 
-    TinyGPSDate date = gps.date;
-    TinyGPSTime time = gps.time;
+        feedGpsSomeData(GPS_FEED_TIME); 
+        if(!isGPSDataValid())
+        {
+            print_oled("FAIL INIT FILE: GPS INVALID");
+            Serial.println("Fail init file: GPS data not valid");
+            return false;
+        } 
+        TinyGPSDate date = gps.date;
+        TinyGPSTime time = gps.time;
     
-    filename = String("/") + String(date.month()) + String("-") + String(date.day()) + String("-") 
-                + String(date.year()) + String("$") + String(time.hour())+ String("-") + String(time.minute())
-                + String("-") + String(time.second()) + String(".csv");
-
-    if(!SD.exists(filename))
-    {
-        File file = SD.open(filename, FILE_WRITE, true);
-        // see loop()
-        file.println("date,time,PM2.5,PM10,lat,lng,alt,bmeAlt,humidity,pressure,temp");
-        file.close();
+    
+        filename = "/" + String(date.month()) + "-" + String(date.day()) + "-" 
+                    + String(date.year()) + "$" + String(time.hour())+ "-" + String(time.minute())
+                    + "-" + String(time.second()) + ".csv";
     }
+    else
+    {
+        int counter = 0;
+        do { counter++; filename = "/" + String(counter) + ".csv"; }
+        while(SD.exists(filename));
+    }
+
+    File file = SD.open(filename, FILE_WRITE, true);
+    // see loop()
+    file.println("date,time,PM2.5,PM10,lat,lng,alt,bmeAlt,humidity,pressure,temp");
+    file.close();
+
     return true;
 }
 
@@ -269,7 +333,7 @@ static void shutDown()
     print_oled("SHUT DOWN");
     SD.end();
     Serial.end();
-    Serial1.end();
+    if(IS_GPS_PRESENT) Serial1.end();
     Serial2.end();
     while(true);
 }
@@ -357,10 +421,36 @@ static void writeFile(const char* path, String& message)
 
 void IRAM_ATTR isr_button()
 {
+    a = 1;
     unsigned long time = millis();
-    if (time - shutdownBtn.last_debounce_time > 250)
+    if (time - btn1.last_debounce_time > 250)
     {
-        shutdownBtn.pressed = true;
-        shutdownBtn.last_debounce_time = time;
+        btn1.pressed = true;
+        btn1.last_debounce_time = time;
     }
 }
+
+void setupWiFi()
+{
+
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(local_ip, gateway, subnet);
+    WiFi.softAP(ssid, password);
+
+    Serial.print("WiFi AP created: ");
+    Serial.println(WiFi.softAPIP());
+}
+
+bool initFTP()
+{
+    ftpServer.addUser(FTP_USER, FTP_PASSWORD);
+    ftpServer.addFilesystem("SD", &SD);
+    if (!ftpServer.begin())
+    {
+        Serial.println("FTP server creation error");
+        return false;
+    }
+    Serial.println("FTP Server created");
+    return true;
+}
+
